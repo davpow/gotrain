1a808286c83d591c5e57dc76734e7b73ebdd01e6
dxpowell <dxpowell@gmail.com>
2015-08-30 21:41:56 -0400

    GOT-20150830:  more types all

---
+++ src/letsgo/moretypes/nil-slices.go
@@ -0,0 +1,17 @@
+/**
+Nil slices
+The zero value of a slice is nil.
+
+A nil slice has a length and capacity of 0.
+ */
+package main
+
+import "fmt"
+
+func main() {
+	var z []int
+	fmt.Println(z, len(z), cap(z))
+	if z == nil {
+		fmt.Println("nil!")
+	}
+}
---
+++ src/letsgo/moretypes/struct-pointers.go
@@ -0,0 +1,21 @@
+/**
+Pointers to structs
+Struct fields can be accessed through a struct pointer.
+
+The indirection through the pointer is transparent.
+ */
+package main
+
+import "fmt"
+
+type Vertex struct {
+	X int
+	Y int
+}
+
+func main() {
+	v := Vertex{1, 2}
+	p := &v
+	p.X = 1e9
+	fmt.Println(v)
+}
